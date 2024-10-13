a8f6464f2de18bdcdc116a2608e003fb7ffa883d
Nikolai Poriadin <np25071984@gmail.com>
2024-10-13 16:38:20 -0400

    Track group brand name as iterate over group items in Avangard provider

--- source/app/Converters/AvangardUsdConverter.php
+++ source/app/Converters/AvangardUsdConverter.php
@@ -10,7 +10,7 @@ readonly class AvangardUsdConverter extends AbstractConverter
     private const int INDEX_ARTICLE = 0;
     private const int INDEX_TITLE = 1;
     private const int INDEX_PRICE = 2;
-    private const int FIRST_ROW = 8;
+    private const int FIRST_ROW = 7;
 
     public function convert(Spreadsheet $spreadsheet): array
     {
@@ -18,14 +18,24 @@ readonly class AvangardUsdConverter extends AbstractConverter
         $activeSheet = $spreadsheet->getActiveSheet();
         $highestRow = $activeSheet->getHighestRow();
         $rows = $activeSheet->rangeToArray(sprintf("B%d:D%d", self::FIRST_ROW, $highestRow));
-        foreach ($rows as $r) {
+        $currentBrand = null;
+        foreach ($rows as $i => $r) {
             if (empty($r[self::INDEX_ARTICLE])) {
+                if (empty($r[self::INDEX_TITLE])) {
+                    // the last row is empty row
+                    continue;
+                }
+                $currentBrand = $this->normolizeString($r[self::INDEX_TITLE]);
                 continue;
+            }
+            $title = $this->normolizeString($r[self::INDEX_TITLE]);
+            if (mb_substr($title, 0, mb_strlen($currentBrand)) !== $currentBrand) {
+                $title = $currentBrand . " " . $title;
             }
             $price = (float)trim($r[self::INDEX_PRICE]);
             $data[] = new RawPriceListItem(
                 article: trim($r[self::INDEX_ARTICLE]),
-                title: $this->normolizeString($r[self::INDEX_TITLE]),
+                title: $title,
                 price: $price,
             );
         }
